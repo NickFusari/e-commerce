a69309477c8a9365b3ab48b7ae5cf0d76a779ace
Nicolas Fusari <nick.fusari96@gmail.com>
2023-09-10 10:04:37 +0200

    buttons are working on products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommerceService } from '../commerce.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products',
@@ -8,5 +9,42 @@ import { CommerceService } from '../commerce.service';
 })
 export class ProductsComponent {
 
-  constructor(public service: CommerceService){}
+  toBeDisplayed: Array<Product> = new Array<Product>();
+
+  constructor(public service: CommerceService){
+
+    setTimeout(()=>{
+      this.toBeDisplayed = this.service.products;
+      console.log(this.toBeDisplayed);
+    }, 1000);
+  }
+
+  checkDisplay(x: number){
+    switch (x){
+      case 1:
+        this.toBeDisplayed = this.service.gadgets;
+        break;
+      case 2:
+        this.toBeDisplayed = this.service.beauty;
+        break;
+      case 3:
+        this.toBeDisplayed = this.service.groceries;
+        break;
+      case 4:
+        this.toBeDisplayed = this.service.homedeco;
+        break;
+      case 5:
+        this.toBeDisplayed = this.service.womens;
+        break;
+      case 6:
+        this.toBeDisplayed = this.service.mens;
+        break;
+      case 7:
+        this.toBeDisplayed = this.service.sunglasses;
+        break;
+      case 8:
+        this.toBeDisplayed = this.service.products;
+        break;
+    }
+  }
 }
